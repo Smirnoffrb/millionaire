8f4def37e00f3554aa7071df7d4991dfcd825f1d
smirnoff <smrnvmail@gmail.com>
2022-07-14 19:29:13 +0300

    test current_game_guestion and previous_level model Game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -106,4 +106,18 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:money)
     end
   end
+
+  describe '.current_game_question' do
+    it 'return current game question' do
+      expect(game_w_questions.current_game_question).to eq(game_w_questions.game_questions[0])
+    end
+  end
+
+  describe '.previous_level' do
+    let(:level) { game_w_questions.current_level }
+
+    it 'return correct level' do
+      expect(game_w_questions.previous_level).to eq(level - 1)
+    end
+  end
 end
